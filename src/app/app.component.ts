082b67d9d08fd9d8d250bba621f2615f643eac71
Zach Neill <zacharyneill@gmail.com>
2022-08-06 17:22:27 -0400

    Added bare bones styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'game';
+export class AppComponent implements OnInit {
+  centerText = "This is the home page"
+
+  ngOnInit(){
+    setInterval(()=>this.centerText+=" page", 100)
+  }
 }
